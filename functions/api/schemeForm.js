30dc656e427b5914ce407309e412baa54aaa8167
vikasjangid_mail <vikasjangid.mai@gmail.com>
2025-12-11 08:13:29 +0530

    edited

--- functions/api/schemeForm.js
+++ functions/api/schemeForm.js
@@ -11,6 +11,7 @@ export async function onRequestPost(context) {
     const TO_EMAIL = context.env.TO_EMAIL;
 
     // Debug: Check if env variables exist
+    //and also check
     if (!RESEND_API_KEY) {
         return new Response(
             JSON.stringify({ success: false, error: "RESEND_API_KEY not set" }),
